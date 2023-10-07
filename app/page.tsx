684d990747c90d2426e9971a6c11d3288f2b899d
Priaum Talukder <priamcse@outlook.com>
2023-10-07 22:32:53 +0600

    env sample

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const session = await getServerSession(authConfig);
-  console.log(session);
+  // console.log(session);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {session ? <>Welcome Back {session?.user?.name}</> : <LoginForm />}
